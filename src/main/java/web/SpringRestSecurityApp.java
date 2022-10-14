bb6dd08fe0884e8cdfedd47ff1c04629f6264506
yuliy-smrnv <yuliy.smrnv@gmail.com>
2022-10-14 21:44:16 +0300

    init commit

---
+++ src/main/java/web/SpringRestSecurityApp.java
@@ -0,0 +1,12 @@
+package web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSecurityApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSecurityApp.class, args);
+    }
+}
